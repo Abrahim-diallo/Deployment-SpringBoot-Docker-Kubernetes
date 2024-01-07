a6cacf1d19af1f48ca62cf5a6ca8fa2223a273b9
Abraham <diallo.abraham@outlook.com>
2024-01-07 21:59:14 +0100

    Coding: Config Project.

---
+++ Deployment-SpringBoot-Docker-Kubernetes/src/main/java/com/brahimcodester/BrahimCodesterApplication.java
@@ -0,0 +1,13 @@
+package com.brahimcodester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrahimCodesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrahimCodesterApplication.class, args);
+	}
+
+}
